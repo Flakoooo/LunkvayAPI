bd2d08e99e9b911558d1b5581aad2871c85a0e68
Flakoooo <vlasov.vadom@gmail.com>
2025-11-03 20:19:56 +0500

    db update

---
+++ mainapi/Data/Migrations/20251103151806_ReturnImages.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LunkvayAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReturnImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "img_db_delete_url",
+                table: "chat_images");
+
+            migrationBuilder.DropColumn(
+                name: "img_db_id",
+                table: "chat_images");
+
+            migrationBuilder.DropColumn(
+                name: "img_db_delete_url",
+                table: "avatars");
+
+            migrationBuilder.DropColumn(
+                name: "img_db_id",
+                table: "avatars");
+
+            migrationBuilder.RenameColumn(
+                name: "img_db_url",
+                table: "chat_images",
+                newName: "file_name");
+
+            migrationBuilder.RenameColumn(
+                name: "img_db_url",
+                table: "avatars",
+                newName: "file_name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "file_name",
+                table: "chat_images",
+                newName: "img_db_url");
+
+            migrationBuilder.RenameColumn(
+                name: "file_name",
+                table: "avatars",
+                newName: "img_db_url");
+
+            migrationBuilder.AddColumn<string>(
+                name: "img_db_delete_url",
+                table: "chat_images",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "img_db_id",
+                table: "chat_images",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "img_db_delete_url",
+                table: "avatars",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "img_db_id",
+                table: "avatars",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
